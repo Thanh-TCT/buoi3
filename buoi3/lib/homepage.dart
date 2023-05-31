ce3e0b7698f33cddbce14e8c26fd8fd0b0c3d09e
Thanh-TCT <trancongthanh032000@gmail.com>
2023-05-31 22:32:26 +0700

    update buoi3

--- buoi3/lib/homepage.dart
+++ buoi3/lib/homepage.dart
@@ -18,6 +18,7 @@ class _homepageState extends State<homepage> {
 
   List<String> listNameStudent = [];
   List<String> listGenderStudent = [];
+  Color ColorBtn = Colors.white;
 
   @override
   void initState() {
@@ -90,53 +91,100 @@ class _homepageState extends State<homepage> {
             child: ListView.builder(
                 itemCount: listNameStudent.length,
                 itemBuilder: (context, index) {
-                  return Row(
-                    children: [
-                      SizedBox(
-                        height: 60,
-                        child: Container(
-                          child: Container(
-                            height: 30,
-                            alignment: Alignment.topCenter,
-                            margin: EdgeInsets.all(10),
-                            decoration:
-                                const BoxDecoration(shape: BoxShape.circle),
-                            child: ClipOval(
-                                child: Image(
-                                    image: NetworkImage(
-                                        'https://th.bing.com/th/id/OIP.nMIr-nB5v0rbPzWEJzeZcQHaE7?pid=ImgDet&rs=1'))),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Container(
-                            color: Colors.blueGrey,
-                            child: ListTile(
-                              title: Text(
-                                listNameStudent[index],
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
-                              subtitle: (Text(
-                                listGenderStudent[index],
-                                style: TextStyle(
-                                    color: Colors.white30, fontSize: 15),
-                              )),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                  return BoxItem(
+                    nameStudent: listNameStudent[index],
+                    genderStudent: listGenderStudent[index],
                   );
                 }),
           ),
         ),
       ]),
+    );
+  }
+}
+
+class BoxItem extends StatefulWidget {
+  const BoxItem({
+    super.key,
+    required this.nameStudent,
+    required this.genderStudent,
+  });
+
+  final String nameStudent;
+  final String genderStudent;
+
+  @override
+  State<BoxItem> createState() => _BoxItemState();
+}
+
+class _BoxItemState extends State<BoxItem> {
+  bool click = false;
+  Color ColorBtn = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 60,
+          child: Container(
+            child: Container(
+              height: 30,
+              alignment: Alignment.topCenter,
+              margin: EdgeInsets.all(10),
+              decoration: const BoxDecoration(shape: BoxShape.circle),
+              child: ClipOval(
+                  child: Image(
+                      image: NetworkImage(
+                          'https://th.bing.com/th/id/OIP.nMIr-nB5v0rbPzWEJzeZcQHaE7?pid=ImgDet&rs=1'))),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+              color: Colors.blueGrey,
+              child: ListTile(
+                title: Text(
+                  widget.nameStudent,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                subtitle: (Text(
+                  widget.genderStudent,
+                  style: TextStyle(color: Colors.white30, fontSize: 15),
+                )),
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          thickness: 50,
+          color: Colors.black,
+          indent: 50,
+          height: 30,
+        ),
+        Container(
+          child: InkWell(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(),
+                  color: ColorBtn),
+              child: Icon(click == false ? Icons.add : Icons.check, size: 30),
+            ),
+            onTap: () {
+              ColorBtn = click == false ? Colors.red : Colors.white;
+              setState(() {
+                click = !click;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
